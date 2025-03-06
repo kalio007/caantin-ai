ff59207d93eb130ba1bbc2590ff2d3dad289a9ee
kalio007 <kalioprince1@gmail.com>
2025-03-06 22:38:53 +0100

    updated flow text

--- src/components/SidePanel/AppSideBar.tsx
+++ src/components/SidePanel/AppSideBar.tsx
@@ -5,7 +5,7 @@ export function AppSidebar() {
   return (
     <div className="p-4 w-full">
       <div className="text-lg font-semibold mb-4 text-center">
-        Supported Nodes
+        Build a New Workflow
       </div>
       <div className="space-y-4">
         <CreateNode />
